c76de0afb462e9280f842caf9da97d00e54f602e
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2020-11-26 09:27:49 -0300

    Commit

--- public/js/admin/index.js
+++ public/js/admin/index.js
@@ -1,1 +1,61 @@
 const socket = Socket()
+
+//define some sample data
+var tabledata = [
+	{
+		userName: 'José',
+		userColor: 'test',
+		userID: 't39pehwueowe3',
+		userIP: '123.345.234.745',
+		socketID: 'sfis98oe983oeise',
+		onlineTime: 1606332084009
+	},
+	{
+		userName: 'Jeff',
+		userColor: 'green',
+		userID: 't39pehwueowe3',
+		userIP: '123.345.234.745',
+		socketID: 'sfis98oe983oeise',
+		onlineTime: 1606332084009
+	},
+	{
+		userName: 'José',
+		userColor: 'blue',
+		userID: 't39pehwueowe3',
+		userIP: '123.345.234.745',
+		socketID: 'sfis98oe983oeise',
+		onlineTime: 1606332084009
+	},
+	{
+		userName: 'José',
+		userColor: 'red',
+		userID: 't39pehwueowe3',
+		userIP: '123.345.234.745',
+		socketID: 'sfis98oe983oeise',
+		onlineTime: 1606332084010
+	},
+]
+
+var table = new Tabulator('#example-table', {
+	data: tabledata,
+	resizableColumns: false,
+	selectablePersistence: true,
+	columns: [
+		{ headerSort: false, formatter: 'rowSelection', titleFormatter: 'rowSelection', cellClick: (e, cell) => cell.getRow().toggleSelect(), headerClick: (e, column) => console.log(column.getTable().selectRow('visible')) },
+		{ title: 'Nome', field: 'userName', headerFilter:'input', formatter: cell => { cell.getElement().style.color = cell.getRow().getData().userColor; return cell.getValue() } },
+		//{ title: 'Cor', field: 'userColor', formatter: 'color', headerFilter:"input" },
+		{ title: 'ID', field: 'userID', headerFilter:"input" },
+		{ title: 'IP', field: 'userIP', headerFilter:"input" },
+		{ title: 'Socket ID', field: 'socketID', headerFilter:"input" },
+		{
+			title: 'Online desde', field: 'onlineTime', formatter: cell => moment(cell.getValue()).format('HH:MM:SS DD/MM/YYYY') },
+		{
+			title: 'Ações', headerSort: false, formatter: function (cell, formatterParams, onRendered) {
+				button = document.createElement('button')
+				button.innerText = 'CLICK'
+				button.onclick = () => alert(cell.getData().userName)
+				return button
+			}
+		},
+	]
+})
