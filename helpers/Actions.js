8dab63b031846635636e3c95ff534bc7ac9c51da
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-01-27 10:04:31 -0300

    OneSignal backend

--- helpers/Actions.js
+++ helpers/Actions.js
@@ -2,6 +2,7 @@ const MAX_MESSAGES = 300
 const { typingUsers } = require('./Chat')
 const chat = require('./Chat')
 const safeData = require('./SafeData')
+const fetch = require('node-fetch')
 
 function Actions(io) {
 	function userConnected(socket, user) {
@@ -35,6 +36,28 @@ function Actions(io) {
 		io.of('/chat').emit('chat', safeData.message(message))
 		io.of('/admin').emit('chat', message)
 		typing(socket, message.sender, false)
+		
+		// Notificação
+		if (chat.messages.length) {
+			let message = ''
+			chat.messages.slice(-4).forEach((m, i) => {
+				if (i !== 0) message += '\n'
+				message += `"${m.sender.userName}": ${m.text}`
+			})
+			fetch('https://onesignal.com/api/v1/notifications', {
+				method: 'POST',
+				headers: {
+					'Authorization': 'Basic YWZjNjQ0ZDYtNzg5MC00ZWJiLWIxZDEtZjg2ZjkwMTliMjk4',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					'included_segments': ['Subscribed Users'],
+					'app_id': '816dc5a8-149a-4f41-a2e8-2bb933c59e56',
+					'contents': { en: message },
+					'template_id': '7ccaaaf9-1183-4d8f-aa42-413319e4cd6d'
+				})
+			})
+		}
 	}
 
 	function typing(socket, user, typing) {
